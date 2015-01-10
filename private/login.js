45a542b02286f22427004ab53ebd5b49f49b1495
Grant Timmerman <granttimmerman@gmail.com>
2015-01-10 14:59:16 -0800

    Add more auth routes

--- private/login.js
+++ private/login.js
@@ -4,7 +4,8 @@ module.exports = function (passport) {
   passport.use(new FacebookStrategy({
       clientID: process.env.FACEBOOK_APP_ID,
       clientSecret: process.env.FACEBOOK_APP_SECRET,
-      callbackURL: process.env.FACEBOOK_CALLBACK_URL
+      callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+      profileFields: ['id', 'emails', 'displayName', 'photos']
     },
     function(accessToken, refreshToken, profile, done) {
       User.findOrCreate({ facebookId: profile.id }, function (err, user) {
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,11 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.authError = function(req, res) {
+  res.send('error');
+};
+
+exports.authSuccess = function(req, res) {
+  res.send('good');
+};
