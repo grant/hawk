9ff515e0bc5733deeeba8a8d3782b372eac59c0a
Grant Timmerman <granttimmerman@gmail.com>
2015-01-11 12:10:30 -0800

    Add map zoom to location

--- public/js/index.js
+++ public/js/index.js
@@ -36,20 +36,36 @@ $(function () {
       }
     });
 
+    var map;
     function initialize() {
-      var mapCanvas = document.getElementById('map');
-      var mapOptions = {
-        panControl: false,
-        zoomControl: false,
-        mapTypeControl: false,
-        scaleControl: false,
-        streetViewControl: false,
-        overviewMapControl: false,
-        center: new google.maps.LatLng(44.5403, -78.5463),
-        zoom: 12,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      };
-      var map = new google.maps.Map(mapCanvas, mapOptions);
+
+      function updateCenter () {
+        $.get('/api/location', function (location) {
+          map.setCenter(google.maps.LatLng(location.lat, location.lng));
+        });
+      }
+
+      // Setup first time
+      $.get('/api/location', function (location) {
+        console.log(location);
+        var mapCanvas = document.getElementById('map');
+        var mapOptions = {
+          panControl: false,
+          zoomControl: false,
+          mapTypeControl: false,
+          scaleControl: false,
+          streetViewControl: false,
+          overviewMapControl: false,
+          center: new google.maps.LatLng(location.lat, location.lng),
+          zoom: 12,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map = new google.maps.Map(mapCanvas, mapOptions);
+      });
+
+      setInterval(function () {
+        updateCenter();
+      }, 15000);
     }
     google.maps.event.addDomListener(window, 'load', initialize);
   }
