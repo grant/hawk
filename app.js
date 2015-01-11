1f28bf8fe3b4e8a9528f6f42b92b0a28a1ac2464
Grant Timmerman <granttimmerman@gmail.com>
2015-01-11 11:26:44 -0800

    Delay to 15 sec crawl

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ setInterval(function () {
   mojio.getData(function (data) {
     // do nothing
   });
-}, 3000);
+}, 15000);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
