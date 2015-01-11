1a0817c867bcc7982380e61f4452e86822a67e37
Grant Timmerman <granttimmerman@gmail.com>
2015-01-11 10:16:25 -0800

    Add getData method

--- mojio/index.js
+++ mojio/index.js
@@ -7,59 +7,65 @@ var AuthKey = '0d53368f-1280-4f4c-b16b-ea191ec4c6a3',
 	HOME_COORD = {lat: 37.571633, lng: -122.418558},
 	RADIUS = 10;
 
-var getdata = function(req, res) {
+var getDataRoute = function (req, res) {
+	getData(function (data) {
+		cb(data);
+	});
+};
+
+var getData = function (cb) {
 	request({
-			url: 'http://data.api.hackthedrive.com:80/v1/Events?limit=1&offset=0&sortBy=Time&desc=true',
-			headers: {
-				'MojioAPIToken' : AuthKey
-			}
-		}, function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-				var rawdata = JSON.parse(body).Data[0];
-				// TripSchema.findOne({TripId : rawdata.TripId}, function(err, trip){
-				// 	if(!trip){
-				// 		var trip = {
+		url: 'http://data.api.hackthedrive.com:80/v1/Events?limit=1&offset=0&sortBy=Time&desc=true',
+		headers: {
+			'MojioAPIToken' : AuthKey
+		}
+	}, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var rawdata = JSON.parse(body).Data[0];
+			// TripSchema.findOne({TripId : rawdata.TripId}, function(err, trip){
+			// 	if(!trip){
+			// 		var trip = {
 
-				// 		}
-				// 	}
-				// });
+			// 		}
+			// 	}
+			// });
 
-				var tripsData = {
-					TripId: rawdata.TripId,
-					Speed: rawdata.Speed,
-					Distance: rawdata.Distance,
-					Odometer: rawdata.Odometer,
-					FuelEfficiency: rawdata.FuelEfficiency,
-					SteeringWheelAngle: rawdata.SteeringWheelAngle,
-					Gear: rawdata.Gear,
-					Location: rawdata.Location,
-					Accelerometer: rawdata.Accelerometer,
-					AcceleratorPedal: rawdata.AcceleratorPedal,
-					Time: rawdata.Time,
-					ConnectionLost: rawdata.ConnectionLost,
-					BatteryLevel: rawdata.BatteryLevel,
-					TemperatureInside: rawdata.TemperatureInside,
-					TemperatureOutside: rawdata.TemperatureOutside
-				};
+			var tripsData = {
+				TripId: rawdata.TripId,
+				Speed: rawdata.Speed,
+				Distance: rawdata.Distance,
+				Odometer: rawdata.Odometer,
+				FuelEfficiency: rawdata.FuelEfficiency,
+				SteeringWheelAngle: rawdata.SteeringWheelAngle,
+				Gear: rawdata.Gear,
+				Location: rawdata.Location,
+				Accelerometer: rawdata.Accelerometer,
+				AcceleratorPedal: rawdata.AcceleratorPedal,
+				Time: rawdata.Time,
+				ConnectionLost: rawdata.ConnectionLost,
+				BatteryLevel: rawdata.BatteryLevel,
+				TemperatureInside: rawdata.TemperatureInside,
+				TemperatureOutside: rawdata.TemperatureOutside
+			};
 
-				getSpeedLimitFunction(rawdata.Location.Lat, rawdata.Location.Lng, 
-					function(speedlim){
-						tripsData.SpeedLimit = speedlim;
-						var recordTripData = new TripDataSchema(tripsData);
-						recordTripData.save(function(error){
-							if(error){
-								console.log(error);
-								res.status(500).json({status: 'fail'});
-							} else {
-								res.json({status: 'pass'});
-							}
-						});
+			getSpeedLimitFunction(rawdata.Location.Lat, rawdata.Location.Lng,
+				function(speedlim){
+					tripsData.SpeedLimit = speedlim;
+					var recordTripData = new TripDataSchema(tripsData);
+					recordTripData.save(function(error){
+						if(error){
+							console.log(error);
+							cb({status: 'fail'});
+						} else {
+							cb({status: 'pass'});
+						}
 					});
-			} else {
-				res.status(500).json({status: 'failtogetdata'});
-			}
-		});
-}
+				});
+		} else {
+			cb({status: 'failtogetdata'});
+		}
+	});
+};
 
 var getalerts = function (req, res) {
 	TripDataSchema.find({}, function(error, data){
@@ -100,7 +106,8 @@ module.exports = {
 
 	getSpeedLimit: getSpeedLimitFunction,
 
-	getData: getdata,
+	getData: getData,
+	getDataRoute: getDataRoute,
 
 	getAlerts: getalerts,
 
@@ -111,7 +118,7 @@ module.exports = {
 	isOutOfRegion: function (home, cur, radius) {
 		return GeoDist(home, cur) - radius > 0;
 	}
-}; 
+};
 
 var getSpeedLimitFunction = function (lat, lng, cb) {
 	var link = 'http://route.st.nlp.nokia.com/routing/6.2/getlinkinfo.json?waypoint='+lat+','+lng+'&app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg';
