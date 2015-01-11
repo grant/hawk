758ef4ae41732c98591cb5044a1859e83948091a
Grant Timmerman <granttimmerman@gmail.com>
2015-01-11 11:45:13 -0800

    Add less transparency to bg

--- public/js/index.js
+++ public/js/index.js
@@ -40,7 +40,7 @@ $(function () {
       var mapCanvas = document.getElementById('map');
       var mapOptions = {
         center: new google.maps.LatLng(44.5403, -78.5463),
-        zoom: 8,
+        zoom: 12,
         mapTypeId: google.maps.MapTypeId.ROADMAP
       };
       var map = new google.maps.Map(mapCanvas, mapOptions);
